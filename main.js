337c918e6fbf11dd4b0b38647107c95c7e68e8fc
Rustambek Mamadaliyev <80699038+Rustambek-Mamadaliyev0301@users.noreply.github.com>
2021-06-05 08:16:55 -0700

    add file

---
+++ main.js
@@ -0,0 +1,72 @@
+
+// let myChart=document.getElementById("myChart").getContext("2d")
+// let barChart=new CharacterData(myChart,{
+//     type:"bar",
+//     data:{
+//         labels:[],
+//         datasets:[]
+//     },
+//     options:{}
+// })
+
+const actions = [
+    {
+      name: 'Randomize',
+      handler(chart) {
+        chart.data.datasets.forEach(dataset => {
+          dataset.data = Utils.numbers({count: chart.data.labels.length, min: -100, max: 100});
+        });
+        chart.update();
+      }
+    },
+    {
+      name: 'Add Dataset',
+      handler(chart) {
+        const data = chart.data;
+        const dsColor = Utils.namedColor(chart.data.datasets.length);
+        const newDataset = {
+          label: 'Dataset ' + (data.datasets.length + 1),
+          backgroundColor: Utils.transparentize(dsColor, 0.5),
+          borderColor: dsColor,
+          borderWidth: 1,
+          data: Utils.numbers({count: data.labels.length, min: -100, max: 100}),
+        };
+        chart.data.datasets.push(newDataset);
+        chart.update();
+      }
+    },
+    {
+      name: 'Add Data',
+      handler(chart) {
+        const data = chart.data;
+        if (data.datasets.length > 0) {
+          data.labels = Utils.months({count: data.labels.length + 1});
+  
+          for (var index = 0; index < data.datasets.length; ++index) {
+            data.datasets[index].data.push(Utils.rand(-100, 100));
+          }
+  
+          chart.update();
+        }
+      }
+    },
+    {
+      name: 'Remove Dataset',
+      handler(chart) {
+        chart.data.datasets.pop();
+        chart.update();
+      }
+    },
+    {
+      name: 'Remove Data',
+      handler(chart) {
+        chart.data.labels.splice(-1, 1); // remove the label first
+  
+        chart.data.datasets.forEach(dataset => {
+          dataset.data.pop();
+        });
+  
+        chart.update();
+      }
+    }
+  ];
